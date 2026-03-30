08b770840f17b145bdeab4f460bb26639cb08106
Giovanni Iadelise <iadelisegiovanni2000@gmail.com>
2026-03-30 10:42:03 +0200

    Add files via upload

---
+++ animali/animali/src/main/java/it/adozioni/animali/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.adozioni.animali;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AnimaliApplication.class);
+	}
+
+}
