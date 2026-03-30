e50bd6a69712480f42ea5e18d71f4fcd87fd30a7
giova <giovanni14.piccolo03@gmail.com>
2026-03-30 15:08:12 +0200

    Changes: VisitaMedicaRepository.java Add: AbstractController.java

--- animali/animali/src/main/java/it/adozioni/animali/Repository/VisitaMedicaRepository.java
+++ animali/animali/src/main/java/it/adozioni/animali/Repository/VisitaMedicaRepository.java
@@ -4,4 +4,5 @@ import it.adozioni.animali.Model.VisitaMedica;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VisitaMedicaRepository extends JpaRepository<VisitaMedica, Long> {
+
 }
