ef9168c2681402d3edc66f128aa516d474dc8eba
giova <giovanni14.piccolo03@gmail.com>
2026-03-30 12:33:02 +0200

    add: VisitaMedica

--- animali/animali/src/main/java/it/adozioni/animali/model/VisitaMedica.java
+++ animali/animali/src/main/java/it/adozioni/animali/model/VisitaMedica.java
@@ -1,5 +1,33 @@
 package it.adozioni.animali.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "VisitaMedica", schema = "Animali")
 public class VisitaMedica {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String nome;
+
+    LocalDateTime data;
+
+    String esito;
+
+    String veterinario;
+
+    @ManyToOne
+    @JoinColumn(name = "animale_id", nullable = true)
+    private Animale animale;
 
 }
