cc94650f83885e1f9595f430cbb177b56cfa3c3f
giova <giovanni14.piccolo03@gmail.com>
2026-03-30 16:05:21 +0200

    Changes: VisitaMedicaController.java

--- animali/animali/src/main/java/it/adozioni/animali/Controller/VisitaMedicaController.java
+++ animali/animali/src/main/java/it/adozioni/animali/Controller/VisitaMedicaController.java
@@ -3,11 +3,10 @@ package it.adozioni.animali.Controller;
 import it.adozioni.animali.Dto.VisitaMedicaDto;
 import it.adozioni.animali.Service.VisitaMedicaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -19,7 +18,19 @@ public class VisitaMedicaController extends AbstractController<VisitaMedicaDto> 
     private VisitaMedicaService service;
 
     @GetMapping("/findByNome")
-    public List<VisitaMedicaDto> findByNome(String nome) {
+    public VisitaMedicaDto findByNome(@RequestParam("nome") String nome) {
         return service.findByNome(nome);
     }
+
+    @GetMapping("/findByData")
+    public List<VisitaMedicaDto> findByData(@RequestParam("data") LocalDateTime data) {
+        return service.findByData(data);
+    }
+
+    @GetMapping("/findByEsito")
+    public List<VisitaMedicaDto> findByEsito(@RequestParam("esito") String esito) {
+        return service.findByEsito(esito);
+    }
+
+    @GetMapping("/findByVeterinario")
 }
