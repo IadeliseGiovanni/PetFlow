1a3f7e58e7fbb0cdd6acad8493c4d25eedf6c32b
Giovanni Iadelise <iadelisegiovanni2000@gmail.com>
2026-03-30 15:57:39 +0200

    Changes AnimaleMapper.java CentroAdozione.java CentroAdozioneController.java CentroAdozioneDto.java CentroAdozioneMapper.java CentroAdozioneService.java compiler.xml Converter.java VisitaMedicaMapper.java VolontarioMapper.java workspace.xml

--- animali/animali/src/main/java/it/adozioni/animali/Mapper/CentroAdozioneMapper.java
+++ animali/animali/src/main/java/it/adozioni/animali/Mapper/CentroAdozioneMapper.java
@@ -2,41 +2,24 @@ package it.adozioni.animali.Mapper;
 
 import it.adozioni.animali.Dto.CentroAdozioneDto;
 import it.adozioni.animali.Model.CentroAdozione;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CentroAdozioneMapper extends AbstractConverter<CentroAdozione, CentroAdozioneDto> {
 
-    @Override
-    public CentroAdozioneDto toDto(CentroAdozione entity) {
-        if (entity == null) {
-            return null;
-        }
-        CentroAdozioneDto dto = new CentroAdozioneDto();
-        dto.setNomeCentro(entity.getNomeCentro());
-        dto.setIndirizzo(entity.getIndirizzo());
-        dto.setCitta(entity.getCitta());
-        dto.setCapacitaMassima(entity.getCapacitaMassima());
-        dto.setIsNoProfit(entity.getIsNoProfit());
-        return dto;
-    }
+public class CentroAdozioneMapper extends AbstractConverter<CentroAdozione, CentroAdozioneDto>  {
 
+    private final ModelMapper modelMapper = new ModelMapper();
+
+    // Converte le liste (necessario per il Service)
     @Override
     public CentroAdozione toEntity(CentroAdozioneDto dto) {
-        if (dto == null) {
-            return null;
-        }
-        CentroAdozione entity = new CentroAdozione();
-        entity.setNomeCentro(dto.getNomeCentro());
-        entity.setIndirizzo(dto.getIndirizzo());
-        entity.setCitta(dto.getCitta());
-        entity.setCapacitaMassima(dto.getCapacitaMassima());
-        entity.setIsNoProfit(dto.getIsNoProfit());
-        return entity;
+        return modelMapper.map(dto,CentroAdozione.class);
     }
 
     @Override
-    public CentroAdozioneDto toDTO(CentroAdozione centroAdozione) {
-        return null;
+    public CentroAdozioneDto toDTO(CentroAdozione entity) {
+        return modelMapper.map(entity,CentroAdozioneDto.class);
     }
-}
+
+    }
--- animali/animali/src/main/java/it/adozioni/animali/Service/CentroAdozioneService.java
+++ animali/animali/src/main/java/it/adozioni/animali/Service/CentroAdozioneService.java
@@ -7,23 +7,16 @@ import it.adozioni.animali.Repository.CentroAdozioneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class CentroAdozioneService {
+public class CentroAdozioneService extends AbstractService<CentroAdozione, CentroAdozioneDto> {
 
     @Autowired
-    private CentroAdozioneRepository repository;
+    public CentroAdozioneService(CentroAdozioneRepository repository, CentroAdozioneMapper mapper) {
+        super(repository, mapper);
+    }
 
-    @Autowired
-    private CentroAdozioneMapper mapper; // Iniettiamo il mapper che hai appena creato
-
-    // Metodo aggiornato: molto più corto e pulito!
-    public List<CentroAdozioneDto> listaTuttiICentri() {
-        // 1. Prendo la lista delle Entity (Model) dal database
-        List<CentroAdozione> modelli = repository.findAll();
-
-        // 2. Uso il metodo ereditato dall'AbstractConverter per convertire tutta la lista in un colpo solo
-        return mapper.toDtoList(modelli);
+    // Metodo extra se vuoi chiamarlo specificamente dal Controller
+    public Iterable<CentroAdozioneDto> listaTuttiICentri() {
+        return getAll();
     }
 }
